86a3559de72595e59d32b6c6e42696b8297de726
Ganesh-186 <ganeshravichandran.06@gmail.com>
2026-06-04 09:33:47 +0530

    Modified

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,5 +2,6 @@ import {test} from '@playwright/test';
 
 
 test('Opening amazon',async({page})=>{
-  await page.goto('https://www.amazon.in/')
+  await page.goto('https://www.amazon.in/');
+  await page.waitForTimeout(4000);
 })
